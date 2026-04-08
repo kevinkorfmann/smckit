3f73d2d5546e9cb094236898abae46d61b70cf86
kevinkorfmann <kevin.korfmann@gmail.com>
2026-04-07 21:15:56 -0400

    Prepare 0.0.1 beta release

--- src/smckit/__init__.py
+++ src/smckit/__init__.py
@@ -4,6 +4,6 @@ from smckit import io, pl, pp, tl, upstream
 from smckit._core import SmcData
 from smckit.settings import settings
 
-__version__ = "0.0.1"
+__version__ = "0.0.1b0"
 
 __all__ = ["SmcData", "io", "pl", "pp", "settings", "tl", "upstream"]
